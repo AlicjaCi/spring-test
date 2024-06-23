d4dbe162fe04bc6db40cbc165c3559e920c5c2e5
DESKTOP-S2OPI5L\alka9 <alka.975@gmail.com>
2024-06-23 16:47:39 +0200

    MYP-44 - method for fixed installment calculation added

---
+++ src/main/java/com/example/demo/LoanCalculator.java
@@ -0,0 +1,10 @@
+package com.example.demo;
+
+public class LoanCalculator {
+
+
+    public static double calculateFixedInstallment(double kwotakredytu, double oprocentowanie, int liczbaRat) {
+        double q = 1 + (oprocentowanie / 12);
+        return (kwotakredytu * oprocentowanie) / (12 * (1 - Math.pow(q, -liczbaRat)));
+    }
+}

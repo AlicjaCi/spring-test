73c88f3eac39056ad3450ed0d88de8fb837f1dc7
DESKTOP-S2OPI5L\alka9 <alka.975@gmail.com>
2024-06-23 17:21:56 +0200

    MYP-45 refactor

--- src/main/java/com/example/demo/LoanCalculator.java
+++ src/main/java/com/example/demo/LoanCalculator.java
@@ -3,8 +3,8 @@ package com.example.demo;
 public class LoanCalculator {
 
 
-    public static double calculateFixedInstallment(double kwotakredytu, double oprocentowanie, int liczbaRat) {
+    public static double calculateFixedInstallment(double loanAmount, double oprocentowanie, int liczbaRat) {
         double q = 1 + (oprocentowanie / 12);
-        return (kwotakredytu * oprocentowanie) / (12 * (1 - Math.pow(q, -liczbaRat)));
+        return (loanAmount * oprocentowanie) / (12 * (1 - Math.pow(q, -liczbaRat)));
     }
 }

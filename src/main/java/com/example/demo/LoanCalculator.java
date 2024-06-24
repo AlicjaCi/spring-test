79e9312b14dd560808560f96abf8934921c54617
AlicjaCi <alka.975@gmail.com>
2024-06-24 19:53:47 +0200

    Update LoanCalculator.java

--- src/main/java/com/example/demo/LoanCalculator.java
+++ src/main/java/com/example/demo/LoanCalculator.java
@@ -4,7 +4,7 @@ public class LoanCalculator {
 
 
     public static double calculateFixedInstallment(double kwotakredytu, double oprocentowanie, int liczbaRat) {
-        double q = 1 + (oprocentowanie / 12);
-        return (kwotakredytu * oprocentowanie) / (12 * (1 - Math.pow(q, -liczbaRat)));
+        double qPerMonth = 1 + (oprocentowanie / 12);
+        return (kwotakredytu * oprocentowanie) / (12 * (1 - Math.pow(qPerMonth, -liczbaRat)));
     }
 }
